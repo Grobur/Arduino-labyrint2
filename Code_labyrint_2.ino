197c9fc3bea2938339dee2ae0677ccd955b7d03a
Grobur <35265661+Grobur@users.noreply.github.com>
2018-01-24 23:40:43 +0100

    Update Code_labyrint_2.ino

--- Code_labyrint_2.ino
+++ Code_labyrint_2.ino
@@ -27,11 +27,11 @@
 #include <Servo.h>  // Library voor servomotoren
 #include <LiquidCrystal_I2C.h> // Library voor LCD-display
 
-#define drukknop A2 // Definieer drukknop voor analoge inputpin A2
+#define drukknop A0 // Definieer drukknop voor analoge inputpin A2
 #define neopin1 11 // Definieer neopixel LED 1 voor outputpin 11
 #define neopin2 10 // Definieer neopixel LED 2 voor outputpin 10
-#define joyX A0 //Definieer X-as van joystick voor analoge inputpin A0
-#define joyY A1 //Definieer Y-as van joystick voor analoge inputpin A1
+#define joyX A1 //Definieer X-as van joystick voor analoge inputpin A0
+#define joyY A2 //Definieer Y-as van joystick voor analoge inputpin A1
 
 Adafruit_NeoPixel neoA = Adafruit_NeoPixel(1, neopin1, NEO_GRB + NEO_KHZ800);
 // Aanmaken van een neopixel genaamd neoA, aangesloten op neopin1
@@ -57,19 +57,28 @@ long roodA; // Creëer variabele voor rode kleur voor Neopixel A
 long groenA; // Creëer variabele voor groene kleur voor Neopixel A
 long roodB; // Creëer variabele voor rode kleur voor Neopixel B
 long groenB; // Creëer variabele voor groene kleur voor Neopixel B
-int servoMaxX;
-int servoMinX;
+long zwartA; // Creëer variabele om Neopixel A uit te schakelen
+long zwartB; // Creëer variabele om Neopixel B uit te schakelen
+long oranjeA; // Creëer variabele voor oranje kleur voor Neopixel A
+long oranjeB; // Creëer variabele voor oranje kleur voor Neopixel B
+int servoMaxX; // Creëer variabelen om minimum- en maximumwaarden van het aantal graden van de servo's in
+int servoMinX; // X en Y richting te bepalen
 int servoMaxY;
 int servoMinY;
 int poortWaarde; // Creëer variabele voor ofwel groene (Winst) ofwel rode (Verlies) poort
 int sTijd; // Creëer variabele om begintijd van een ronde in op te slaan voor scoremultiplier op einde
 int eTijd; // Creëer variabele om eindtijd van een ronde in op te slaan voor scoremultiplier op einde
 int eind; // Creëer variabele om de eindstand van een ronde in op te slaan (Winst of Verlies)
-long score; // Creëer variabele om score aan het einde van een ronde in op te slaan
-long vScore; // Creëer variabele om de score van de vorige ronde in op te slaan
-long tScore; // Creëer variabele om de totaalscore in op te slaan
+int ronde = 0; // Creëer variabele om te bepalen of er al een vorige ronde is gespeeld (begin bij 0 voor geen vorige ronde)
+float tijdMultiplier; // Creëer variabele om multiplier voor score op basis van verstreken milliseconden per
+// ronde op te slaan
+float score; // Creëer variabele om de float-score van de huidige ronde in op te slaan
+unsigned long sScore; //Creëer variabele om vereenvoudigde score (natuurlijk getal) in op te slaan
+unsigned long tScore; // Creëer variabele om de totaalscore in op te slaan
 
 void setup() {
+
+  Serial.begin(9600);
 
   lcd.init();
   lcd.backlight();
@@ -79,10 +88,14 @@ void setup() {
   ServoY.attach(5);
   neoA.begin(); // Initialiseer Neopixel 1
   neoB.begin(); // Initialiseer Neopixel 2
-  roodA = neoA.Color(100, 0, 0);
+  roodA = neoA.Color(150, 0, 0);
+  roodB = neoB.Color(150, 0, 0);
   groenA = neoA.Color(0, 100, 0);
-  roodB = neoB.Color(100, 0, 0);
   groenB = neoB.Color(0, 100, 0);
+  oranjeA = neoA.Color(150, 100, 0);
+  oranjeB = neoB.Color(150, 100, 0);
+  zwartA = neoA.Color(0, 0, 0);
+  zwartB = neoB.Color(0, 0, 0);
   randomSeed(analogRead(6));
 
 }
@@ -92,11 +105,13 @@ void loop() {
   knopstat = digitalRead(drukknop);
   lcd.clear();
 
-  while (knopstat == LOW) {
+  while (knopstat == LOW && ronde == 0) {
     /* Alle commando's binnen deze while loop hebben als doel
       twee verschillende teksten op het LCD-scherm te tonen met een interval van 3 seconden.
       De millis() functie wordt gebruikt zodat de gebruiker op ieder moment op de drukknop
-      kan drukken zonder dat het volledige programma onderbroken wordt door een delay().*/
+      kan drukken zonder dat het volledige programma onderbroken wordt door een delay().
+      Deze tekst wordt enkel tijdens het initiële starten van het programma getoond, na een
+      eerste ronde wordt deze tekst overgeslagen.*/
 
     tijdNu = millis();
 
@@ -105,6 +120,10 @@ void loop() {
       lcd.print("Welkom bij het");
       lcd.setCursor(0, 1);
       lcd.print("Knikkerlabyrint!");
+      neoA.setPixelColor(0, oranjeA);
+      neoB.setPixelColor(0, oranjeB);
+      neoA.show();
+      neoB.show();     
 
     }
 
@@ -114,6 +133,10 @@ void loop() {
       lcd.print("Druk op de knop");
       lcd.setCursor(0, 1);
       lcd.print("om te starten!");
+      neoA.setPixelColor(0, zwartA);
+      neoB.setPixelColor(0, zwartB);
+      neoA.show();
+      neoB.show();
 
     }
 
@@ -122,13 +145,16 @@ void loop() {
       tijdNet = millis();
 
     }
-
+    ServoX.write(90);
+    ServoY.write(90);
     knopstat = digitalRead(drukknop);
   }
 
   while (knopstat == HIGH) {
 
     knopstat = digitalRead(drukknop);
+    ServoX.write(90);
+    ServoY.write(90);
 
   }
 
@@ -136,10 +162,12 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("Spel start over");
 
-  for (teller = 5; teller > 0; teller --) {
+  for (teller = 5; teller > 0; teller --) { // Deze for loop telt 5 seconden op de LCD af vóór het spel start
 
     lcd.setCursor(0, 1);
     lcd.print(teller);
+    ServoX.write(90);
+    ServoY.write(90);
 
     if (teller > 1) {
 
@@ -159,10 +187,14 @@ void loop() {
 
   }
 
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("START!");
+
   servoMaxX = 120; // Definieer de graden die de servo kan afleggen wanneer de joystick bediend wordt
   servoMinX = 60;   // Deze waarden zijn variabelen opdat een powerup de besturing van de servo's kan
-  servoMaxY = 120;  // omkeren.
-  servoMinY = 60;
+  servoMaxY = 170;  // omkeren.
+  servoMinY = 110;
 
   poortWaarde = random(0, 2);
   // Kies een willekeurige waarde (0 tot 1) om de poortkleuren (groen of rood) voor deze ronde te bepalen
@@ -177,18 +209,20 @@ void loop() {
   neoA.show();
   neoB.show();
 
-  ServoX.write(90);
-  ServoY.write(90);
+  arrayChecker(); // Deze functie bepaalt de waarden van de array voor de lichtsensoren
 
-  arrayChecker();
-  
-  sTijd = millis();
+  sTijd = millis(); // Bepaal de starttijd van de ronde
 
-  while (lichtArray[1] > 400 && lichtArray[2] > 400 && lichtArray[5] > 400) {
+  while (lichtArray[0] > 800 && lichtArray[1] > 400 && lichtArray[4] > 400) { // Zolang de twee poortsensoren en de sensor
+    // onderaan in de trechter ononderbroken belicht worden, is hun waarde > 700 en blijft deze while loop doorgaan.
+    // Als de knikker voorbij één van deze sensoren rolt, wordt deze loop onderbroken en wordt de eindstand bepaald.
 
     arrayChecker();
 
-    joyWaardeX = analogRead(joyX);
+    joyWaardeX = analogRead(joyX); // Deze commando's zorgen ervoor dat de analoge inputwaarden van de joystick gemapt
+      // worden naar de minimum- en maximumwaarden van de servo's. Deze waarden worden opgeslagen in de variabele
+      //joyWaardeX (en Y) en die wordt op zijn beurt naar de servo geschreven. Op die manier loopt de servo synchroon
+      //met de inputwaarde van de joystick.
     joyWaardeX = map(joyWaardeX, 8, 890, servoMinX, servoMaxX);
     ServoX.write(joyWaardeX);
 
@@ -196,29 +230,33 @@ void loop() {
     joyWaardeY = map(joyWaardeY, 8, 894, servoMinY, servoMaxY);
     ServoY.write(joyWaardeY);
 
-    if (lichtArray[3] < 400 || lichtArray[4] < 400) {
+    if (lichtArray[2] < 300 || lichtArray[3] < 300) { // Deze if statement controleert of de knikker over één van de
+      // twee powerup-lichtsensoren in het speelveld rolt
 
       powerup = random(0, 2); // Bepaal een willekeurige waarde voor de powerup (0 of 1)
 
-      lcd.setCursor(0, 0);
-      lcd.print("Jouw Powerup:");
       if (powerup == 0) { // Deze powerup keert de besturing van de servo's om
         servoMinX = 120;
         servoMaxX = 60;
         servoMinY = 120;
         servoMaxY = 60;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Jouw Powerup:");
         lcd.setCursor(0, 1);
         lcd.print("Omkering!");
+
       }
 
       else { // Deze powerup verandert de poorten van kleur, zodat de speler de bal
-        // naar de tegenovergestelde poort moet bewegen
+        // naar de tegenovergestelde poort moet bewegen. De omkering gebeurt door de poortWaarde, bepaald aan
+        // het begin van de ronde, uit te lezen.
         if (poortWaarde == 0) {
           neoA.setPixelColor(0, groenA);
           neoB.setPixelColor(0, roodB);
           neoA.show();
           neoB.show();
-          poortWaarde = 1;
+          poortWaarde = 1; // Afhankelijk van de oorspronkelijke waarde van poortWaarde wordt deze nu omgedraaid.
         }
         else {
           neoA.setPixelColor(0, roodA);
@@ -228,24 +266,30 @@ void loop() {
           poortWaarde = 0;
         }
 
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Jouw Powerup:");
         lcd.setCursor(0, 1);
         lcd.print("Andere poort!");
 
       }
-    }
+
+   }
   }
 
-  eTijd = millis() - sTijd;
+  eTijd = millis() - sTijd; // De eindtijd wordt bepaald op basis van het huidige aantal milliseconden minus het aantal
+  // milliseconden (opgeslagen in sTijd) bij de start van de ronde
 
-  ServoX.write(90);
-  ServoY.write(90);
   lcd.clear();
   lcd.setCursor(0, 0);
-  if (lichtArray[1] <= 400) {
+  if (lichtArray[0] <= 800) { // Als de knikker in een valkuil in het speelveld valt, zal deze onmiddellijk in de
+    // trechter onderaan rollen en deze lichtsensor triggeren, die de boodschap "VERLIES!" op de display weergeeft 
     lcd.print("VERLIES!");
+    eind = 0; // De eindstand wordt opgeslagen in deze variabele voor de verdere scorebepaling
   }
 
-  else if (lichtArray[2] <= 400) {
+  else if (lichtArray[1] <= 400) { // Als de knikker in een poort rolt, zal deze de lichtsensor activeren en afhankelijk
+    // van de eerder bepaalde poortWaarde zal dit een "Win" of "Loss" uitkomst teweeg brengen. 
     if (poortWaarde == 0) {
       lcd.print("OVERWINNING!");
       eind = 1;
@@ -256,7 +300,7 @@ void loop() {
     }
   }
 
-  else if (lichtArray[5] <= 400) {
+  else if (lichtArray[4] <= 400) { // Ook de lichtsensor in de andere poort wordt op een analoge manier gecontroleerd.
     if (poortWaarde == 0) {
       lcd.print("FOUTE POORT!");
       eind = 0;
@@ -267,28 +311,56 @@ void loop() {
     }
   }
 
-  delay(3000);
-  lcd.clear();
-  if (eind == 0) {
-    score = 0;
+  if (eind = 0) {
+    neoA.setPixelColor(0, roodA);
+    neoB.setPixelColor(0, roodB);
+    neoA.show();
+    neoB.show();
   }
 
   else {
-    score = 100 / eTijd + 100;
+    neoA.setPixelColor(0, groenA);
+    neoB.setPixelColor(0, groenB);
+    neoA.show();
+    neoB.show();
   }
 
-  lcd.setCursor(0, 0);
-  lcd.print("Huidige score:");
-  lcd.setCursor(0, 1);
-  lcd.print(score);
+  delay(3000);
+  lcd.clear();
+  if (eind == 0) { // Als de eindstand 0 is, zullen beide LEDs in de poorten rood oplichten om het verlies extra
+    // kracht bij te zetten
+    sScore = 0; // De huidige rondescore wordt op 0 gezet
+    neoA.setPixelColor(0, roodA);
+    neoB.setPixelColor(0, roodB);
+    neoA.show();
+    neoB.show();
+  }
 
-  tScore = score + vScore;
-  vScore = score;
+  else {
+    tijdMultiplier = 1000. / (float)eTijd; // De tijdsbonus wordt berekend door 1000 te delen door het aantal milliseconden
+    // dat deze ronde duurde. Dit resulteert in een kommagetal en om dit op te slaan wordt er gebruik gemaakt van een float.
+    score = (float)tijdMultiplier * 1000. + 100.; // De eigenlijke score wordt berekend door bij een vaste score van 100 de
+    // tijdsbonus, opgeslagen in "tijdMultiplier", te vermenigvuldigen met 1000 en vervolgens op te tellen. Kortere ronde-
+    // tijden geven vanzelfsprekend hogere bonusscores
+    sScore = score; // De floatscore wordt vereenvoudigd naar een natuurlijk getal, decimalen zijn namelijk overbodig bij
+    // de weergave van een score
+    neoA.setPixelColor(0, groenA);
+    neoB.setPixelColor(0, groenB);
+    neoA.show();
+    neoB.show();
+  }
+
+  lcd.setCursor(0, 0); // Op het display wordt de huidige score weergegeven door "sScore" te printen
+  lcd.print("Huidige score:"); 
+  lcd.setCursor(0, 1);
+  lcd.print(sScore);
+
+  tScore = sScore + tScore; // De totale score wordt berekend door de huidige score op te tellen bij de totaalscore
+  // van de vorige ronde(s). Na de eerste ronde is de totaalscore aan de rechterkant dus gelijk aan 0
 
   delay(3000);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0); // De totaalscore "tScore" wordt op de display weergegeven
   lcd.print("Totale score:");
   lcd.setCursor(0, 1);
   lcd.print(tScore);
@@ -297,7 +369,13 @@ void loop() {
 
   lcd.clear();
 
-  while (knopstat == LOW) {
+  neoA.setPixelColor(0, zwartA); // Na deze score 3 seconden weergegeven te hebben, wordt het scherm gewist en de Neo-
+  // pixels uitgeschakeld
+  neoB.setPixelColor(0, zwartB);
+  neoA.show();
+  neoB.show();
+
+  while (knopstat == LOW) { // Deze while loop blijft van kracht tot de drukknop wordt ingedrukt
     knopstat = digitalRead(drukknop);
     lcd.setCursor(0, 0);
     lcd.print("Druk om verder");
@@ -305,13 +383,13 @@ void loop() {
     lcd.print("te gaan!");
   }
 
-  while (knopstat == HIGH) {
+  while (knopstat == HIGH) { // Deze while loop fungeert als een pauze zolang de gebruiker de drukknop ingedrukt houdt
     knopstat = digitalRead(drukknop);
   }
 
   delay(50);
   knopstat = digitalRead(drukknop);
-  tijdNet = millis();
+  ronde = 1; // De variabele ronde wordt gelijkgesteld aan 1 zodat de eerste while loop na deze ronde niet meer doorgaat
 }
 
 void arrayChecker() {
